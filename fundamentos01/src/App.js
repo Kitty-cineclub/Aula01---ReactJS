99360edda05c2e69660cf08fc65de0f79d7f8443
Kitty-cineclub <Kathy.lima.mene@hotmail.com>
2021-04-10 21:06:15 +0000

    Aula 01 - Primeiro Componente

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import Mundo from './components/basico/Mundo'
+
+class App extends Component{
+render() {
+return(
+  <div>
+    <h1>Aula 1 - Fundamentos 01</h1>
+    
+    <Mundo />
+  
+  </div>
+    ) 
+  }
+}
+
+export default App;
---
+++ fundamentos01/src/components/basico/Mundo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+    
+    const informacao = 'primeiro componente jsx'
+    
+    return(
+        <>
+            <h2>componente olaMundo</h2>
+            <p>{ informacao }</p>
+        </>
+    )
+}

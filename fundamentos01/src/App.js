8d2c5386f140705dce12234fd95a79e8cd7de72e
Kitty-cineclub <Kathy.lima.mene@hotmail.com>
2021-04-11 01:41:27 +0000

    part2

--- fundamentos01/src/App.js
+++ fundamentos01/src/App.js
@@ -1,17 +1,28 @@
 import React, {Component} from 'react'
+import './App.css'
+import Card from'./components/layout/Card'
+import Propriedades from './components/basico/Propriedades'
 import Mundo from './components/basico/Mundo'
 
-class App extends Component{
-render() {
-return(
-  <div>
-    <h1>Aula 1 - Fundamentos 01</h1>
+class App extends Component {
+  render() {
+    return(
+      <div className='App'>
+      <h1>Exemplos de React</h1>
     
-    <Mundo />
-  
+      <div className='Cards'>
+
+          <Card titulo='Propriedades'>
+              <Propriedades nome="Homer" sobrenome="jay Simpson" idade={38}/>
+          </Card>
+    
+          <Card titulo='Olá mundo!'>
+              <Mundo />
+          </Card>
+    
+    </div>
   </div>
     ) 
   }
 }
-
 export default App;
